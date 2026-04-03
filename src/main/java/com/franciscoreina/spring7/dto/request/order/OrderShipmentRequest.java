3e7ecfc252e10e53cfb0c3ff05c994aaf6a85da7
Francisco Reina <franciscoreinaponce@gmail.com>
2026-04-03 11:17:34 +0100

    Clean up order shipment request

--- src/main/java/com/franciscoreina/spring7/dto/request/order/OrderShipmentRequest.java
+++ src/main/java/com/franciscoreina/spring7/dto/request/order/OrderShipmentRequest.java
@@ -1,13 +1,9 @@
 package com.franciscoreina.spring7.dto.request.order;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-@Builder
 public record OrderShipmentRequest(
 
         @NotBlank
