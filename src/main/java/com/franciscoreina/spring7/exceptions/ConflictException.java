dcfcc0ce5476992be789fe90188567682058e7f8
Francisco Reina <franciscoreinaponce@gmail.com>
2026-04-03 10:51:34 +0100

    Clean up exceptions

--- src/main/java/com/franciscoreina/spring7/exceptions/ConflictException.java
+++ src/main/java/com/franciscoreina/spring7/exceptions/ConflictException.java
@@ -5,8 +5,4 @@ public class ConflictException extends RuntimeException {
     public ConflictException(String message) {
         super(message);
     }
-
-    public ConflictException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
